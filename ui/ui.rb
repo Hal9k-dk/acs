7cee8c904f07392e86bc610528ade92e66d1fe74
Torsten Martinsen <torsten@bullestock.net>
2020-08-20 17:52:27 +0200

    Set stdout to flush immediately.

--- ui/ui.rb
+++ ui/ui.rb
@@ -4,6 +4,8 @@ require 'serialport'
 require 'rest-client'
 require 'pg'
 
+$stdout.sync = true
+
 HOST = 'https://127.0.0.1'
 
 LED_ENTER = 'P250R8SGN'
@@ -642,11 +644,17 @@ ui.set_reader(reader)
 puts("----\nReady")
 ui.clear();
 
-wdog = File.open('/dev/watchdog', 'w')
+USE_WDOG = true
+
+if USE_WDOG
+  wdog = File.open('/dev/watchdog', 'w')
+end
 
 while true
   ui.update()
   reader.update()
   sleep 0.1
-  wdog.ioctl(0x80045705) # WDIOC_KEEPALIVE
+  if USE_WDOG
+    wdog.ioctl(0x80045705) # WDIOC_KEEPALIVE
+  end
 end
