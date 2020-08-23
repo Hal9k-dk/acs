39fba9ba517ee6c4bbac2dc946e0cb400e534681
Torsten Martinsen <torsten@bullestock.net>
2020-08-23 17:21:47 +0200

    Do not use String.

--- reader/firmware/libs/RDM6300/RDM6300.h
+++ reader/firmware/libs/RDM6300/RDM6300.h
@@ -3,18 +3,17 @@
 class RDM6300
 {
 public:
+    /// Size of ID in bytes
+    static const int ID_SIZE = 6;
+    
     RDM6300()
-        : m_state(0),
-          m_checksum(0),
-          m_temp(0),
-          m_index(0)
     {
     }
     
     bool add_byte(uint8_t input)
     {
 #ifdef PROTOCOL_DEBUG
-        Serial.print("Add ");Serial.println((int) input);
+        Serial.print(F("Add "));Serial.println((int) input);
 #endif
         if (input == STX)
         {
@@ -22,7 +21,7 @@ public:
             m_checksum = 0;
             m_index = 0;
 #ifdef PROTOCOL_DEBUG
-            Serial.println("State 1");
+            Serial.println(F("State 1 (got STX)"));
 #endif
         }
         else if (m_state == 1)
@@ -32,7 +31,7 @@ public:
                 m_temp -= 7;
             m_state = 2;
 #ifdef PROTOCOL_DEBUG
-            Serial.print("State 2: "); Serial.println(m_temp);
+            Serial.print(F("State 2: ")); Serial.println(m_temp);
 #endif
         }
         else if (m_state == 2)
@@ -42,26 +41,41 @@ public:
                 input -= 7;
             m_temp = (m_temp << 4) | input;
 #ifdef PROTOCOL_DEBUG
-            Serial.print("Store "); Serial.print(m_temp); Serial.print(" at "); Serial.println(m_index);
+            Serial.print(F("Store ")); Serial.print(m_temp); Serial.print(F(" at ")); Serial.println(m_index);
 #endif
             m_buf[m_index++] = m_temp;
-            if (m_index > 5)
+            if (m_index >= ID_SIZE)
+            {
                 m_state = 3; 
+#ifdef PROTOCOL_DEBUG
+                Serial.println(F("State 3"));
+#endif
+            }
             else
             { 
                 m_state = 1; 
 #ifdef PROTOCOL_DEBUG
-                Serial.print("State 1: "); Serial.println(m_temp);
+                Serial.print(F("State 1: ")); Serial.println(m_temp);
 #endif
                 m_checksum ^= m_temp;
             }
         }
-        else if ((m_state == 3) && (input == ETX))
-        { 
-            m_state = 4; 
+        else if (m_state == 3)
+        {
+            if (input == ETX)
+            { 
+                m_state = 4; 
 #ifdef PROTOCOL_DEBUG
-            Serial.println("State 4");
+                Serial.println(F("State 4"));
 #endif
+            }
+            else
+            { 
+                m_state = 0; 
+#ifdef PROTOCOL_DEBUG
+                Serial.println(F("Error: Expected ETX"));
+#endif
+            }
         }
         else
             m_state = 0;
@@ -73,25 +87,23 @@ public:
         return false;
     }
 
-    String get_id() const
+    const char* get_id() const
     {
-        String s;
         for (int i = 0; i < ID_SIZE; ++i)
         {
-            char buf[5];
-            sprintf(buf, "%02X", m_buf[i]);
-            s.concat(buf);
+            sprintf(m_id + i * 2, "%02X", m_buf[i]);
         }
-        return s;
+        m_id[ID_SIZE * 2] = 0;
+        return m_id;
     }
     
 private:
     static const char STX = 2;
     static const char ETX = 3;
-    static const int ID_SIZE = 5;
-    int m_state;
-    int m_checksum;
-    int m_temp;
-    int m_index;
+    int m_state = 0;
+    int m_checksum = 0;
+    int m_temp = 0;
+    int m_index = 0;
     unsigned char m_buf[ID_SIZE];
+    mutable char m_id[ID_SIZE * 2 + 1 + 20]; //!!
 };
