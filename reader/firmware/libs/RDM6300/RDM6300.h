54a62d6a9d694cbba47e17843822d7c57c5c8340
Torsten Martinsen <torsten@bullestock.net>
2020-08-28 21:08:50 +0200

    Rewrite FSA.

--- reader/firmware/libs/RDM6300/RDM6300.h
+++ reader/firmware/libs/RDM6300/RDM6300.h
@@ -4,7 +4,8 @@ class RDM6300
 {
 public:
     /// Size of ID in bytes
-    static const int ID_SIZE = 6;
+    // 13005D1DA5
+    static const int ID_SIZE = 5;
     
     RDM6300()
     {
@@ -12,6 +13,8 @@ public:
     
     bool add_byte(uint8_t input)
     {
+        // Frame format:
+        // <STX> <digit1> ... <digit10> <checksum1> <checksum2> <ETX>
 #ifdef PROTOCOL_DEBUG
         Serial.print(F("Add "));Serial.println((int) input);
 #endif
@@ -21,77 +24,106 @@ public:
             m_checksum = 0;
             m_index = 0;
 #ifdef PROTOCOL_DEBUG
-            Serial.println(F("State 1 (got STX)"));
+            Serial.println(F("STX: State 1"));
 #endif
         }
-        else if (m_state == 1)
-        { 
-            m_temp = input - '0';
-            if (m_temp > 9)
-                m_temp -= 7;
-            m_state = 2;
-#ifdef PROTOCOL_DEBUG
-            Serial.print(F("State 2: ")); Serial.println(m_temp);
-#endif
-        }
-        else if (m_state == 2)
+        else if (input == ETX)
         {
-            input -= '0';
-            if (input > 9)
-                input -= 7;
-            m_temp = (m_temp << 4) | input;
+            const auto old_state = m_state;
+            m_state = 0;
 #ifdef PROTOCOL_DEBUG
-            Serial.print(F("Store ")); Serial.print(m_temp); Serial.print(F(" at ")); Serial.println(m_index);
+            Serial.println(F("ETX: State 0"));
 #endif
-            m_buf[m_index++] = m_temp;
-            if (m_index >= ID_SIZE)
-            {
-                m_state = 3; 
+            if (old_state == 5)
+                return true;
 #ifdef PROTOCOL_DEBUG
-                Serial.println(F("State 3"));
+            Serial.print(F("Error: Old state ")); Serial.println(old_state);
 #endif
-            }
-            else
-            { 
-                m_state = 1; 
-#ifdef PROTOCOL_DEBUG
-                Serial.print(F("State 1: ")); Serial.println(m_temp);
-#endif
-                m_checksum ^= m_temp;
-            }
-        }
-        else if (m_state == 3)
-        {
-            if (input == ETX)
-            { 
-                m_state = 4; 
-#ifdef PROTOCOL_DEBUG
-                Serial.println(F("State 4"));
-#endif
-            }
-            else
-            { 
-                m_state = 0; 
-#ifdef PROTOCOL_DEBUG
-                Serial.println(F("Error: Expected ETX"));
-#endif
-            }
         }
         else
-            m_state = 0;
-        if (m_state == 4)
         {
-            m_state = 0;
-            return true;
+            switch (m_state)
+            {
+            case 1:
+                // Store 1st digit
+                m_temp = input - '0';
+                if (m_temp > 9)
+                    m_temp -= 7;
+                m_state = 2;
+#ifdef PROTOCOL_DEBUG
+                Serial.print(F("State 2: ")); Serial.println(m_temp);
+#endif
+                break;
+
+            case 2:
+                // Store one byte
+                input -= '0';
+                if (input > 9)
+                    input -= 7;
+                m_temp = (m_temp << 4) | input;
+#ifdef PROTOCOL_DEBUG
+                Serial.print(F("Store ")); Serial.print(m_temp); Serial.print(F(" at ")); Serial.println(m_index);
+#endif
+                m_buf[m_index++] = m_temp;
+                if (m_index >= ID_SIZE)
+                {
+                    m_state = 3; 
+#ifdef PROTOCOL_DEBUG
+                    Serial.println(F("State 3"));
+#endif
+                }
+                else
+                {
+                    m_state = 1; 
+#ifdef PROTOCOL_DEBUG
+                    Serial.print(F("State 1: ")); Serial.println(m_temp);
+#endif
+                }
+                break;
+
+            case 3:
+                // Store 1st digit of checksum
+                m_checksum = input - '0';
+                if (m_checksum > 9)
+                    m_checksum -= 7;
+                m_state = 4;
+#ifdef PROTOCOL_DEBUG
+                Serial.print(F("State 3: ")); Serial.println(m_state);
+#endif
+                break;
+
+            case 4:
+                // Store checksum
+                input -= '0';
+                if (input > 9)
+                    input -= 7;
+                m_checksum = (m_checksum << 4) | input;
+                m_state = 5;
+#ifdef PROTOCOL_DEBUG
+                Serial.print(F("State 4: ")); Serial.println(m_checksum);
+#endif
+            }
         }
         return false;
     }
 
     const char* get_id() const
     {
+        int cs = 0;
         for (int i = 0; i < ID_SIZE; ++i)
         {
             sprintf(m_id + i * 2, "%02X", m_buf[i]);
+            cs ^= m_buf[i];
+        }
+        if (cs != m_checksum)
+        {
+#ifdef PROTOCOL_DEBUG
+            Serial.print(F("CS error: Exp "));
+            Serial.print(m_checksum);
+            Serial.print(F(" act "));
+            Serial.println(cs);
+#endif
+            return "";
         }
         m_id[ID_SIZE * 2] = 0;
         return m_id;
