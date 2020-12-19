8ed95d811979106c7d0bafa1cf3c0496a1c053c3
Torsten Martinsen <torsten@bullestock.net>
2020-12-19 16:35:54 +0100

    Read URL/key from settings.yml.

--- barn/rest.py
+++ barn/rest.py
@@ -1,23 +1,26 @@
-import requests, json
+import requests, json, yaml
 
 class RestClient:
-
-    URL = 'https://panopticon.hal9k.dk/api/v1/'
-    TOKEN = 'secret'
+    def __init__(self):
+        contents = open('settings.yml')
+        yml = yaml.load(contents, Loader=yaml.SafeLoader)
+        self.url = yml['settings']['resturl']
+        self.token = yml['secrets']['apikey']
+        print("URL %s" % self.url)
     
     def check_card(self, card_id):
-        data = '{ "api_token": "%s", "card_id": "%s" }' % (self.TOKEN, card_id)
-        response = requests.post(self.URL + 'permissions', data=data, headers={"Content-Type": "application/json"}, verify=False)
+        data = '{ "api_token": "%s", "card_id": "%s" }' % (self.token, card_id)
+        response = requests.post(self.url + 'permissions', data=data, headers={"Content-Type": "application/json"}, verify=False)
         if response.status_code != 200:
             return { 'id': 0 }
         return response.json
 
     def log(self, id, msg):
         if id:
-            data = '{ "api_token": "%s", "log": { "user_id": %d, "message": "%s" } }' % (self.TOKEN, id, msg)
+            data = '{ "api_token": "%s", "log": { "user_id": %d, "message": "%s" } }' % (self.token, id, msg)
         else:
-            data = '{ "api_token": "%s", "log": { "message": "%s" } }' % (self.TOKEN, msg)
-        response = requests.post(self.URL + 'logs', data=data, headers={"Content-Type": "application/json"}, verify=False)
+            data = '{ "api_token": "%s", "log": { "message": "%s" } }' % (self.token, msg)
+        response = requests.post(self.url + 'logs', data=data, headers={"Content-Type": "application/json"}, verify=False)
         return response.status_code == 200
         
 if __name__ == "__main__":
