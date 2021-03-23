b8f66ab26c46204d6abd099475007bd7b31ebb7a
Torsten Martinsen <torsten@bullestock.net>
2021-03-23 20:41:04 +0100

    Add erase test.

--- opensmart/test.rb
+++ opensmart/test.rb
@@ -11,17 +11,23 @@ sleep 5
 line = 0
 color = 0
 n = 0
+erase = false
 while true
   sleep 1
   sp.flush_input
   sp.puts "S"
   puts sp.gets
-  sp.printf "T%02d%02d1TEST TEST TEST%d\n" % [line, color, n]
-  puts sp.gets
-  line = line + 1
+  if erase
+    sp.printf "E%02d\n" % line
+  else
+    sp.printf "T%02d%02d1TEST TEST TEST%d\n" % [line, color, n]
+  end
+  #puts sp.gets
+  line = line + (erase ? 2 : 1)
   n = n + 1
   if line >= 6
     line = 0
+    erase = !erase
   end
   color = color + 1
   if color >= 15
