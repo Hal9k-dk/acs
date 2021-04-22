eb12f084c4e4859aae55ef02f70eed73fff0b66d
Torsten Martinsen <torsten@bullestock.net>
2021-04-22 16:06:36 +0000

    Initialise serial ports first.

--- ui/ui.sh
+++ ui/ui.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/bin/ruby /home/hal9k/acs/ui/ui.rb | tee /var/log/acs/ui.log
+/usr/bin/ruby /home/hal9k/acs/ui/init.rb | tee /var/log/acs/ui.log
+/usr/bin/ruby /home/hal9k/acs/ui/ui.rb | tee -a /var/log/acs/ui.log
