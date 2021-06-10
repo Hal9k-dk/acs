a579f2cc7fedaacb492c59be341dc5a586ea0b70
Torsten Martinsen <torsten@bullestock.net>
2021-06-10 18:22:28 +0200

    Add.

---
+++ barn/slack.py
@@ -0,0 +1,28 @@
+import requests
+
+class Slack:
+
+    def __init__(self):
+        file = open('slack-token')
+        self.token = file.read()
+
+    def set_status(self, status):
+        if status != self.last_status:
+            self.send_message(status)
+            self.last_status = status
+
+    def get_status(self):
+        return self.last_status
+  
+    def send_message(self, msg):
+        print("SLACK: %s" % msg)
+        body = { 'channel': "monitoring", 'icon_emoji': ":panopticon:", 'parse': "full", "text": msg }
+        headers = {
+            'content_type': "application/json",
+            "Authorization": "Bearer %s" % self.token
+        }
+        r = requests.post(url = "https://slack.com/api/chat.postMessage", data = body, headers = headers)
+
+if __name__ == "__main__":
+    s = Slack()
+    s.send_message("Hej fra laden")
