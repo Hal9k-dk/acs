d71d22c4971bca367bd13f2ebb9ef167e250ec4b
Torsten Martinsen <torsten@bullestock.net>
2021-07-12 23:34:30 +0200

    Update for MariaDB.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,49 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230162345) do
+ActiveRecord::Schema.define(version: 0) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "logs", force: :cascade do |t|
-    t.datetime "stamp"
+  create_table "logs", id: :integer, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.datetime "stamp",     default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.string   "message"
-    t.integer  "logger_id", null: false
+    t.integer  "logger_id",                                      null: false
     t.integer  "user_id"
   end
 
-  create_table "machines", force: :cascade do |t|
+  create_table "machines", id: :integer, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at",                                      null: false
     t.string   "api_token"
   end
 
-  create_table "machines_users", id: false, force: :cascade do |t|
+  create_table "machines_users", id: false, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "machine_id", null: false
     t.integer "user_id",    null: false
   end
 
-  create_table "permissions", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "old_machines_users", id: false, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.integer "machine_id", null: false
+    t.integer "user_id",    null: false
   end
 
-  create_table "permissions_users", id: false, force: :cascade do |t|
+  create_table "old_permissions_users", id: false, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "permission_id", null: false
     t.integer "user_id",       null: false
   end
 
-  create_table "unknown_cards", force: :cascade do |t|
-    t.datetime "stamp"
-    t.string   "card_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "permissions", id: :integer, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.string   "name"
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at",                                      null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "permissions_users", id: false, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.integer "permission_id", null: false
+    t.integer "user_id",       null: false
+  end
+
+  create_table "unknown_cards", id: :integer, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.datetime "stamp",      default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.string   "card_id"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  create_table "users", id: :integer, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "fl_id"
     t.integer "member_id"
     t.string  "display_name"
